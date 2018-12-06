5ebefb350c123025eb526eb53f52db20a6911ff0
Plesca Sergiu <44344305+PlescaSergiu@users.noreply.github.com>
2018-12-06 22:32:49 +0200

    Update Student_Interface.java

--- src/student/Student_Interface.java
+++ src/student/Student_Interface.java
@@ -2,12 +2,9 @@ package student;
 
 public interface Student_Interface {
 
-
-
     void addStudent(Long CNP,char sex, String firstName, String lastName, int year, int month, int day);
 
     void deleteStudent(Long CNP);
-
     void listStudent();
     void listByFirstNameAndBirthday();
 
