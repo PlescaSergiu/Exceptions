907db3e9c22fdaaec0a3c64676028a3c7771c8cc
PlescaSergiu <sergiuplesca1993@gmail.com>
2018-12-07 20:34:19 +0200

    ver 1.0

--- src/student/Constructor_Student.java
+++ src/student/Constructor_Student.java
@@ -1,15 +1,7 @@
 package student;
 
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.logging.Logger;
-
-import static java.util.logging.Logger.getLogger;
-
 
 public class Constructor_Student extends Student_Catalog {
-
-    private static Logger LOGGER = getLogger(Constructor_Student.class.getName());
 
     private char sex;
     private String firstName;
@@ -18,36 +10,21 @@ public class Constructor_Student extends Student_Catalog {
     private int month;
     private int day;
     private String birthday;
-    private Long CNP;
 
-    public Constructor_Student() {
+
+    Constructor_Student() {
     }
 
-    public Constructor_Student(Long CNP, char sex, String firstName, String lastName, int year, int month, int day) {
-
-        try {
-            this.CNP = CNP;
-            this.sex = sex;
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.year = year;
-            this.month = month;
-            this.day = day;
-            validateCNP(CNP, year);
-            validateAge(year);
-            validateFirstNameAndLastName(firstName);
-            validateFirstNameAndLastName(lastName);
-            validateSex(sex, CNP);
-
-        } catch (ValidationException e) {
-            System.out.println(e.getMessage());
-            LOGGER.info("Something was wrong");
-
-
-        }
-
+    Constructor_Student(char sex, String firstName, String lastName, int year, int month, int day) {
+        this.sex = sex;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.year = year;
+        this.month = month;
+        this.day = day;
 
     }
+
 
     public char getSex() {
         return sex;
@@ -74,13 +51,10 @@ public class Constructor_Student extends Student_Catalog {
     }
 
 
-    public String getBirthday() {
+    private String getBirthday() {
         return birthday = (year + "/" + month + "/" + day);
     }
 
-    public Long getCNP() {
-        return CNP;
-    }
 
     @Override
     public String toString() {
@@ -92,62 +66,5 @@ public class Constructor_Student extends Student_Catalog {
                 '}';
     }
 
-    private void validateCatalog(boolean valid) throws ValidationException {
-        if (!valid) {
-            LOGGER.info("can create");
-
-            throw new ValidationException("Cant add student to catalog");
-        }
-    }
-
-    private void validateCNP(Long CNP, int year) throws ValidationException {
-
-        if (CNP == null || !Arrays.asList(13).contains(String.valueOf(CNP).length())) {
-            LOGGER.info("user introduced incorect CNP");
-
-            throw new ValidationException("Invalid CNP length");
-        }
-        if (String.valueOf(CNP).matches("[0-9]")) {
-            throw new ValidationException("Invalid CNP length");
-        }
-        if (String.valueOf(CNP).charAt(1) != String.valueOf(year).charAt(2) || String.valueOf(CNP).charAt(2) != String.valueOf(year).charAt(3)) {
-            throw new ValidationException("CNP eror");
-        }
-    }
-
-    private void validateSex(char sex, long CNP) throws ValidationException {
-        if (((Character.toUpperCase(sex)) != 'M') && (Character.toUpperCase(sex) != 'F')) {
-            LOGGER.info("The first CNP digit does not match the gender");
-            throw new ValidationException("Sex must be M (male) or F(female)");
-        }
-        if (((Character.toUpperCase(sex)) == ('M')) && String.valueOf(CNP).charAt(0) != '1') {
-            LOGGER.info("The first CNP digit does not match the gender");
-
-            throw new ValidationException("For men CNP need to start with 1");
-        }
-        if (((Character.toUpperCase(sex)) == 'F') && String.valueOf(CNP).charAt(0) != '2') {
-            LOGGER.info("The first CNP digit does not match the gender");
-
-            throw new ValidationException("For female CNP need to start with 2");
-        }
-    }
-
-    private void validateFirstNameAndLastName(String str) throws ValidationException {
-        if (str == null || (str.length() < 3 || str.length() > 13)) {
-            LOGGER.info("User introduce a incorect first/lastname format");
-            throw new ValidationException("Enter valid first name");
-        }
-        if (!str.matches("[A-Z][a-zA-Z]*")) {
-            LOGGER.info("First/lastname must contains only aphabetic letters ");
-            throw new ValidationException("First name must contains only aphabetic letters");
-        }
-    }
-
-    private void validateAge(int year) throws ValidationException {
-        if ((year > LocalDate.now().getYear() - 18) || year < 1900) {
-            LOGGER.info("Age is over or below");
-            throw new ValidationException("A wrong year");
-        }
-    }
 
 }
--- src/student/Student_Catalog.java
+++ src/student/Student_Catalog.java
@@ -10,8 +10,10 @@ import static java.util.logging.Logger.getLogger;
 
 public class Student_Catalog implements Student_Interface {
 
-    private static Logger LOGGER = getLogger(Student_Catalog.class.getName());
 
+
+    private static Logger LOGGER = getLogger(Student_Catalog.class.getName());
+    private boolean valid;
     private int age; // calculate with method calculateAge
 
     private Map<Long, Constructor_Student> studentsCatalog = new HashMap<>();
@@ -19,9 +21,27 @@ public class Student_Catalog implements Student_Interface {
 
     @Override
     public void addStudent(Long CNP, char sex, String firstName, String lastName, int year, int month, int day) {
-        Constructor_Student student = new Constructor_Student();
+        valid = true;
+        try {
 
-        studentsCatalog.put(CNP, new Constructor_Student(CNP, sex, firstName, lastName, year, month, day));
+            validateCNP(CNP, year);
+            validateAge(year);
+            validateFirstNameAndLastName(firstName);
+            validateFirstNameAndLastName(lastName);
+            validateSex(sex, CNP);
+            validateCatalog(valid);
+            studentsCatalog.put(CNP, new Constructor_Student(sex, firstName, lastName, year, month, day));
+
+
+        } catch (ValidationException e) {
+            valid=false;
+            System.out.println(e.getMessage());
+            LOGGER.info("Something was wrong");
+
+
+        }
+
+
 
     }
 
@@ -54,6 +74,11 @@ public class Student_Catalog implements Student_Interface {
     public int getAge() {
         return age;
     }
+    private void validateCatalog(boolean valid) throws  ValidationException{
+        if (!valid ){
+            throw new ValidationException("Cant create this student object");
+        }
+    }
 
     private void validateDeletStudent(Long CNP) throws ValidationException {
         if (CNP == null || !studentsCatalog.containsKey(CNP)) {
@@ -62,6 +87,56 @@ public class Student_Catalog implements Student_Interface {
             throw new ValidationException("You cant delete this student");
         }
 
+    }
+
+    private void validateCNP(Long CNP, int year) throws ValidationException {
+
+        if (CNP == null || !Arrays.asList(13).contains(String.valueOf(CNP).length())) {
+            LOGGER.info("user introduced incorect CNP");
+
+            throw new ValidationException("Invalid CNP length");
+        }
+        if (String.valueOf(CNP).matches("[0-9]")) {
+            throw new ValidationException("Invalid CNP length");
+        }
+        if (String.valueOf(CNP).charAt(1) != String.valueOf(year).charAt(2) || String.valueOf(CNP).charAt(2) != String.valueOf(year).charAt(3)) {
+            throw new ValidationException("CNP eror");
+        }
+    }
+
+    private void validateSex(char sex, long CNP) throws ValidationException {
+        if (((Character.toUpperCase(sex)) != 'M') && (Character.toUpperCase(sex) != 'F')) {
+            LOGGER.info("The first CNP digit does not match the gender");
+            throw new ValidationException("Sex must be M (male) or F(female)");
+        }
+        if (((Character.toUpperCase(sex)) == ('M')) && String.valueOf(CNP).charAt(0) != '1') {
+            LOGGER.info("The first CNP digit does not match the gender");
+
+            throw new ValidationException("For men CNP need to start with 1");
+        }
+        if (((Character.toUpperCase(sex)) == 'F') && String.valueOf(CNP).charAt(0) != '2') {
+            LOGGER.info("The first CNP digit does not match the gender");
+
+            throw new ValidationException("For female CNP need to start with 2");
+        }
+    }
+
+    private void validateFirstNameAndLastName(String str) throws ValidationException {
+        if (str == null || (str.length() < 3 || str.length() > 13)) {
+            LOGGER.info("User introduce a incorect first/lastname format");
+            throw new ValidationException("Enter valid first name");
+        }
+        if (!str.matches("[A-Z][a-zA-Z]*")) {
+            LOGGER.info("First/lastname must contains only aphabetic letters ");
+            throw new ValidationException("First name must contains only aphabetic letters");
+        }
+    }
+
+    private void validateAge(int year) throws ValidationException {
+        if ((year > LocalDate.now().getYear() - 18) || year < 1900) {
+            LOGGER.info("Age is over or below");
+            throw new ValidationException("A wrong year");
+        }
     }
 
 
